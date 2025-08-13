83260e902b83a99533f6903e4714d0d5f73712a3
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-13 19:01:36 -0400

    correct the import

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const nunito = Nunito({
   variable: "--font-nunito",
   subsets: ["latin"],
@@ -30,11 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${nunito.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={` ${nunito.variable} antialiased`}>{children}</body>
     </html>
   );
 }
