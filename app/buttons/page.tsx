13c2e3bd89101a212f63dfb25de8acd829a93c0e
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-13 19:32:32 -0400

    added some bottons for primary and outline

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -4,8 +4,10 @@ const ButtonPage = () => {
   return (
     <div className="p-4 space-y-4 flex flex-col max-w-[200px]">
       <Button>Default </Button>
-      <Button>primary </Button>
-      <Button>outline primary</Button>
+      <Button variant="primary">primary </Button>
+      <Button variant="primaryOutline">outline primary</Button>
+      <Button variant="primary">secondry</Button>
+      <Button variant="primaryOutline">outline second</Button>
     </div>
   );
 };
