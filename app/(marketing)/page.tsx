b2f8ff166f2d9d22eb5506b64b248ed334d1d55b
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-13 22:09:05 -0400

    compete the footer and the landing page

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,50 @@
+import { Button } from "@/components/ui/button";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  SignUpButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-green-500">hellow</p>
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424] lg:h-[423px] mb-8 lg:mb-0">
+        <Image src="/hero.svg" fill alt="Hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice and master new languages with DDU
+        </h1>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton>
+                <Button size="lg" variant="secondry">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignUpButton>
+                <Button size="lg" variant="primaryOutline">
+                  I already have an account
+                </Button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size="lg" variant="secondry" className="w-full asChild">
+                <Link href={"/learn"}>Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
     </div>
   );
 }
