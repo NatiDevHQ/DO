1b9e219d766d274de345dcc2ad67dcb40e840d0f
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-13 22:46:07 -0400

    mobile reponvide nav

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,4 @@
+import { MobileHeader } from "@/components/mobile-header";
 import { Sidebar } from "@/components/sidebar";
 
 type props = {
@@ -7,6 +8,7 @@ type props = {
 const MainLayout = ({ children }: props) => {
   return (
     <>
+      <MobileHeader />
       <Sidebar className="hidden lg:flex" />
       <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
         <div className="bg-red-500 h-full">{children}</div>
