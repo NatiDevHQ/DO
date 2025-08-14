9e7db95b37153924fc1874c84618ca647fbbe569
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-13 20:08:18 -0400

    create a route group called marketing and add layout in it for rendering children in it

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,15 @@
+type props = {
+  children: React.ReactNode;
+};
+
+const MarketingLayout = ({ children }: props) => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
