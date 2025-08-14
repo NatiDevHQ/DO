003b61193e880cabf55566a944504a3582ceab9e
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-13 22:14:31 -0400

    create the learning page route

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,9 @@
+type props = {
+  children: React.ReactNode;
+};
+
+const MainLayout = ({ children }: props) => {
+  return <div>{children}</div>;
+};
+
+export default MainLayout;
---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,5 @@
+const LearnPage = () => {
+  return <div>page</div>;
+};
+
+export default LearnPage;
