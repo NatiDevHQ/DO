785d0d7274727b30e16f3f2a3bf29bff7bac0d03
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-15 19:32:16 -0400

    added on learn page to dinsplay the clicked flag

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -18,9 +18,9 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={{ title: "spanish", imageSrc: "/es.svg" }}
-          hearts={5}
-          points={100}
+          activeCourse={userProgress.activeCourse}
+          hearts={userProgress.hearts}
+          points={userProgress.points}
           hasActiveSubscription={false}
         />
       </StickyWrapper>
