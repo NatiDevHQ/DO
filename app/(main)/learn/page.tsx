75c075937dc77c23c6339bfb6ae127baf4c5ef5e
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-15 19:36:52 -0400

    update the header to

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -25,7 +25,7 @@ const LearnPage = async () => {
         />
       </StickyWrapper>
       <FeedWrapper>
-        <Header title="spanish" />
+        <Header title={userProgress.activeCourse?.title} />
       </FeedWrapper>
     </div>
   );
