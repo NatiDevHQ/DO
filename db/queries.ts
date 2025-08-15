846d367e269926ce5d808092285638fd785afa4b
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-14 21:16:27 -0400

    now the user progres is saved inthe db with course and the redirect to the active course

--- db/queries.ts
+++ db/queries.ts
@@ -1,7 +1,7 @@
 import { cache } from "react";
 import db from "./drizzle";
 import { auth } from "@clerk/nextjs/server";
-import { userProgress } from "./schema";
+import { courses, userProgress } from "./schema";
 import { eq } from "drizzle-orm";
 
 export const getUserProgress = cache(async () => {
@@ -23,5 +23,13 @@ export const getUserProgress = cache(async () => {
 
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+    //Todo populate units and leassons
+  });
   return data;
 });
--- db/schema.ts
+++ db/schema.ts
@@ -13,13 +13,13 @@ export const coursesRelations = relations(courses, ({ many }) => ({
 
 export const userProgress = pgTable("user_progress", {
   userId: text("user_id").primaryKey(),
-  username: text("user_name").notNull().default("User"),
+  userName: text("user_name").notNull().default("User"),
   userImageSrc: text("user_image_src").notNull().default("/mascot.svg"),
   activeCourseId: integer("active_course_id").references(() => courses.id, {
     onDelete: "cascade",
   }),
   hearts: integer("hearts").notNull().default(5),
-  points: integer("hearts").notNull().default(0),
+  points: integer("points").notNull().default(0),
 });
 
 export const userProgressRelations = relations(userProgress, ({ one }) => ({
