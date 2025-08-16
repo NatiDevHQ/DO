923426b025aa505b8b2c3347ee56c425c11acbac
NATNAEL GETACHEW ASEFA <natigech35@gmail.com>
2025-08-15 20:35:30 -0400

    added all the seeds to db

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -18,7 +18,7 @@ const LearnPage = async () => {
     <div className="flex flex-row-reverse gap-[48px] px-6">
       <StickyWrapper>
         <UserProgress
-          activeCourse={userProgress.activeCourse}
+          activeCourse={userProgress.activeCourse!}
           hearts={userProgress.hearts}
           points={userProgress.points}
           hasActiveSubscription={false}
